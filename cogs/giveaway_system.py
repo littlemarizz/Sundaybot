7ec95b5584fa65f9bc1b671cd0ae9102939de20d
mn2-alishareef <Mohamed2.Alishareef@live.uwe.ac.uk>
2023-09-30 16:41:04 +0500

    Finished most functions of the giveaway command

--- cogs/giveaway_system.py
+++ cogs/giveaway_system.py
@@ -1,6 +1,7 @@
+from typing import Any, Coroutine
 import nextcord as discord
-from nextcord import Interaction, ui
-from nextcord.ui import View, button, Modal, TextInput
+from nextcord import Interaction, ui, Role
+from nextcord.ui import View, button, Modal, TextInput, Button
 import sqlite3
 
 YN_TO_BOOL = {'Y': True, 'N': False}
@@ -9,7 +10,7 @@ ga_db_connection = sqlite3.connect('SundayGiveaways.db')
 ga_cursor = ga_db_connection.cursor()
 
 class UIDModal(Modal):
-    def __init__(self, table: str):
+    def __init__(self, table: str, role_details: tuple):
         super().__init__(title="Provide the information below")
         self.uid = TextInput(
             label="StarRail UID",
@@ -18,29 +19,66 @@ class UIDModal(Modal):
             style=discord.TextInputStyle.short)
         self.add_item(self.uid)
         self.table = table
+        self.role: Role = role_details[0]
+        self.role_entries: int = role_details[1]
 
     async def callback(self, ctx: Interaction):
         # store the UID along with the user's discord ID in the db
-        print(self.table)
-        ga_cursor.execute(f"INSERT INTO {self.table} (discord_id, hsr_uid, entries) VALUES (?, ?, 1)", (ctx.user.id, self.uid.value))
-        ga_db_connection.commit()
-        await ctx.response.send_message(f"**User's UID:** {self.uid.value}")
+        try:
+            if self.role in ctx.user.roles:
+                ga_cursor.execute(f"INSERT INTO {self.table} (discord_id, hsr_uid, entries) VALUES (?, ?, ?)", (ctx.user.id, self.uid.value, self.role_entries))
+            else:
+                ga_cursor.execute(f"INSERT INTO {self.table} (discord_id, hsr_uid, entries) VALUES (?, ?, 1)", (ctx.user.id, self.uid.value))
+            ga_db_connection.commit()
+            await ctx.response.send_message("Joined!", ephemeral=True)
+        except sqlite3.IntegrityError:
+            return await ctx.response.send_message(f"You have already participated in the giveaway!", ephemeral=True)
 
 class GiveawayView(View):
-    def __init__(self, bot = None, uid_required: str = None):
+    def __init__(self, bot = None, uid_required: str = None, special_role: Role = None, special_role_entries: int = None):
         self.bot = bot
         if uid_required:
             self.uid_required = YN_TO_BOOL[uid_required]
+        self.special_role = special_role
+        self.special_role_entries = special_role_entries
         super().__init__(timeout=None)
 
-    @button(label="Join", style=discord.ButtonStyle.blurple)
+    @button(label="Join", style=discord.ButtonStyle.green)
     async def join(self, view: View, ctx: Interaction):
+        # first we need to find out if the user has the booster role
         if self.uid_required:
-            modal = UIDModal(table=self.table)
+            modal = UIDModal(table=self.table, role_details=(self.special_role, self.special_role_entries))
             await ctx.response.send_modal(modal)
         else:
-            await ctx.response.send_message("Joined")
+            try:
+                if self.special_role in ctx.user.roles:
+                    ga_cursor.execute(f"INSERT INTO {self.table} (discord_id, hsr_uid, entries) VALUES (?, ?, ?)", (ctx.user.id, None, self.special_role_entries))
+                else:
+                    ga_cursor.execute(f"INSERT INTO {self.table} (discord_id, hsr_uid, entries) VALUES (?, ?, 1)", (ctx.user.id, None))
+                ga_db_connection.commit()
+                await ctx.response.send_message("Joined!", ephemeral=True)
+            except sqlite3.IntegrityError:
+                return await ctx.response.send_message("You've already joined this Giveaway!", ephemeral=True)
+
+        ga_cursor.execute(f"SELECT COUNT(*) FROM {self.table}")
+        result = ga_cursor.fetchone()
+        participants = result[0]
+
+        self.embed.set_footer(text=f"Number of participants: {participants}")
+        await self.msg.edit(embed=self.embed)
 
     def giveaway_table(self, id):
-        self.table = id
+        if id[:2] == "ga":
+            self.table = id
+        else:
+            self.table = f"ga_{id}"
 
+    def giveaway_message(self, embed: discord.Embed, message: discord.Message):
+        self.embed = embed
+        self.msg = message
+
+
+# class GiveawayManageView(View):
+#     def __init__(self, bot):
+#         self.bot = bot
+#         super().__init__(timeout=None)
--- cogs/giveaways.py
+++ cogs/giveaways.py
@@ -1,8 +1,10 @@
 import nextcord as discord
-from nextcord import slash_command, Interaction, SlashOption, Client
-from nextcord.ext import commands
+from nextcord import slash_command, Interaction, SlashOption, Client, Message
+from nextcord.ext import commands, tasks
 from nextcord.ext.commands import has_permissions
 import sqlite3
+import time
+from random import SystemRandom
 from cogs.giveaway_system import GiveawayView
 
 ga_db_connection = sqlite3.connect('SundayGiveaways.db')
@@ -14,6 +16,12 @@ class Giveaway(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
+        ga_cursor.execute("""CREATE TABLE IF NOT EXISTS giveaways (giveaway_id INTEGER, giveaway_name CHAR, prize TEXT, winners INTEGER, guild_id INTEGER, channel_id INTEGER, end_time INTEGER, ended INTEGER, uid_required TEXT, special_role_id INTEGER, special_role_entries INTEGER);""")
+
+        ga_cursor.execute("""SELECT giveaway_id, winners, guild_id, channel_id, end_time FROM giveaways WHERE ended = 0""")
+        self.active_giveaways = ga_cursor.fetchall()
+
+        self.giveaway_task.start()
         print(f'Bot Loaded | giveaway.py ✅')
 
     @commands.Cog.listener()
@@ -21,30 +29,160 @@ class Giveaway(commands.Cog):
         ga_cursor.close()
         ga_db_connection.close()
 
-    @slash_command(name="giveaway")
+    @slash_command(name="giveaway", description="Create a giveaway.")
     @has_permissions(administrator=True)
     async def setup_giveaway(
         self,
         ctx: Interaction,
         name: str = SlashOption(name="name", description="What's the giveaway about", required=True),
         prize: str = SlashOption(name="prize", description="What's the prize for the Giveaway", required=True),
-        time: str = SlashOption(name="time", description="How long will the giveaway be?", required=True),
+        input_time: str = SlashOption(name="time", description="How long will the giveaway be? (Eg: 30s, 10m, 2h, 7d)", required=True),
+        winners: int = SlashOption(name="num_of_winners", description="How many users are gonna win the giveaway?", required=True),
         uid_requirement: str = SlashOption(name="uid_required", description="Do the users have to enter UID to enter the giveaway? (Y/N)", choices=["Y", "N"], required=True),
-        channel_id: int = SlashOption(name="channel_id", description="Channel ID where you want to post the giveaway", required=False)
+        channel_id: str = SlashOption(name="channel_id", description="Channel ID where you want to post the giveaway", required=False),
+        special_role: str = SlashOption(name="special_role_id", description="If there is a role that would get extra entries for the giveaway", required=False),
+        special_role_entries: int = SlashOption(name="special_role_entries", description="Number of entries that the users' with the role would get", required=False)
         ):
-        em = discord.Embed(title=f"Giveaway - {name}", description=f"**Prize:** {prize} \nGiveaway ends in __. Click the button below to join!")
-        GA_view = GiveawayView(bot=self.bot, uid_required=uid_requirement)
-        if channel_id:
-            channel = await self.bot.fetch_channel(channel_id)
-            ga_msg = await channel.send(embed=em, view=GA_view)
+        if (not special_role and special_role_entries) or (special_role and not special_role_entries):
+            return await ctx.response.send_message("Please enter both `special_role_id` and `special_role_entries`!", ephemeral=True)
+        elif special_role:
+            try:
+                role = discord.utils.find(lambda r: r.id == int(special_role), ctx.channel.guild.roles)
+            except (ValueError, TypeError):
+                return await ctx.response.send_message("Please enter a valid `role`", ephemeral=True)
+
+            if not role:
+                return await ctx.response.send_message("Role not found! Please enter a valid `special_role_id`", ephemeral=True)
+            else:
+                GA_view = GiveawayView(bot=self.bot, uid_required=uid_requirement, special_role=role, special_role_entries=special_role_entries)
         else:
-            ga_msg = await ctx.channel.send(embed=em, view=GA_view)
+            GA_view = GiveawayView(bot=self.bot, uid_required=uid_requirement)
 
-        giveaway_id = f"giveaway_{ga_msg.id}"
+        timez = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+        try:
+            time_in_seconds = int(input_time[:-1]) * timez[input_time[-1].lower()]
+        except (KeyError, ValueError, TypeError):
+            return await ctx.response.send_message("Please enter the `time` in the correct format. Eg: 10s / 20m / 3h / 3d ", ephemeral=True)
+
+        ga_end_time = int(time_in_seconds + time.time())
+        giveaway_embed = discord.Embed(title=f"{name.capitalize()} :tada:", description=f"✧ **Prize:** {prize} \n✧ **Number of Winners:** {winners} \n✧ **Giveaway ends:** <t:{ga_end_time}:R> \nClick the button below to join!")
+        giveaway_embed.color = 0x81d182
+        giveaway_embed.set_author(name=f"Giveaway hosted by {ctx.user.name}", icon_url=ctx.user.avatar.url)
+        giveaway_embed.set_footer(text="Number of participants: 0")
+
+        if channel_id:
+            try:
+                channel = discord.utils.find(lambda c: c.id == int(channel_id), ctx.guild.channels)
+            except (ValueError, TypeError):
+                return await ctx.response.send_message("Please enter a valid `channel_id`", ephemeral=True)
+
+            if not channel:
+                return await ctx.response.send_message("Channel not found! Please enter a valid `channel_id`", ephemeral=True)
+            else:
+                channelID = channel.id
+                ga_msg = await channel.send(embed=giveaway_embed, view=GA_view)
+        else:
+            channelID = ctx.channel.id
+            ga_msg: Message = await ctx.channel.send(embed=giveaway_embed, view=GA_view)
+
+        giveaway_id = f"{ga_msg.id}"
         GA_view.giveaway_table(id=giveaway_id)
+        GA_view.giveaway_message(embed=giveaway_embed, message=ga_msg)
 
-        ga_cursor.execute(""f"CREATE TABLE {giveaway_id} (id INTEGER PRIMARY KEY AUTOINCREMENT, discord_id INTEGER, hsr_uid INTEGER, entries INTEGER)""")
+        if special_role:
+            ga_cursor.execute("INSERT INTO giveaways VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (giveaway_id, name, prize, winners, ctx.guild.id, channelID, ga_end_time, 0, uid_requirement, special_role, special_role_entries))
+        else:
+            ga_cursor.execute("INSERT INTO giveaways VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (giveaway_id, name, prize, winners, ctx.guild.id, channelID, ga_end_time, 0, uid_requirement, None, None))
+
+        ga_cursor.execute(""f"CREATE TABLE ga_{giveaway_id} (id INTEGER PRIMARY KEY AUTOINCREMENT, discord_id INTEGER UNIQUE, hsr_uid INTEGER UNIQUE, entries INTEGER)""")
         ga_db_connection.commit()
 
-        await ctx.response.send_message(f"Done, Giveaway ID: {giveaway_id}", ephemeral=True)
+        self.active_giveaways.append((giveaway_id, winners, ctx.guild.id, channelID, ga_end_time))
+
+        await ctx.response.send_message(f"Done! Giveaway ID: {giveaway_id}", ephemeral=True)
+
+
+    # @slash_command(name="manage_giveaway", description="Use this to manage an already existing giveaway.")
+    # @has_permissions(administrator=True)
+    # async def manage_giveaway(self, ctx: Interaction, id: str = SlashOption(
+    #     name="id", description="The giveaway ID / message ID of the active giveaway.", required=True
+    # )):
+    #     # NEED TO WORK ON THIS
+    #     giveaway_id = f"ga_{id}"
+    #     giveaway_id = int(id)
+    #     GA_manage_view = GiveawayManageView(self.bot)
+    #     return
+
+    @slash_command(name="update_giveaway", description="Use this to update an already existing giveaway.")
+    @has_permissions(administrator=True)
+    async def update_giveaway(self, ctx: Interaction, id: str = SlashOption(
+        name="id", description="The giveaway ID / message ID of the active giveaway.", required=True
+    )):
+        try:
+            ga_cursor.execute("SELECT channel_id, uid_required, special_role_id, special_role_entries FROM giveaways WHERE giveaway_id = ?", (id,))
+            giveaway = ga_cursor.fetchone()
+            channel_id, uid_required, special_role_id, special_role_entries = giveaway[0], giveaway[1], giveaway[2], giveaway[3]
+
+            channel = await self.bot.fetch_channel(channel_id)
+            message = await channel.fetch_message(id)
+
+            updated_GA_view = GiveawayView(bot=self.bot, uid_required=uid_required, special_role=special_role_id, special_role_entries=special_role_entries)
+            updated_GA_view.giveaway_table(id)
+            updated_GA_view.giveaway_message(embed=message.embeds[0], message=message)
+
+            await message.edit(view=updated_GA_view)
+
+            await ctx.response.send_message("Updated the giveaway!", ephemeral=True)
+        except:
+            await ctx.response.send_message("An error occured! Please make sure you're entering the correct giveaway message ID.", ephemeral=True)
+
+
+    @tasks.loop(seconds=2)
+    async def giveaway_task(self):
+        for giveaway in self.active_giveaways:
+            giveaway_id, num_of_winners, guild_id, channel_id, end_time = giveaway[0], giveaway[1], giveaway[2], giveaway[3], giveaway[4]
+            if time.time() >= end_time:
+                # Ended
+                winner_user_ids = self._get_winners(table=f"ga_{giveaway_id}", num_of_winners=num_of_winners)
+                guild = await self.bot.fetch_guild(guild_id)
+                winners = [await guild.fetch_member(winner) for winner in winner_user_ids]
+
+                ga_cursor.execute("""UPDATE giveaways SET ended = 1 WHERE giveaway_id = ?""", (giveaway_id,))
+                ga_db_connection.commit()
+
+                if len(winners) > 1:
+                    giveaway_ended = discord.Embed(title="Giveaway Ended :tada:", description=f"The winners are {', '.join([user.mention for user in winners])} !!!", color=0x81d182)
+                else:
+                    giveaway_ended = discord.Embed(title="Giveaway Ended :tada:", description=f"The winner is {', '.join([user.mention for user in winners])} !!!", color=0x81d182)
+
+                self.active_giveaways.remove(giveaway)
+
+                channel = await self.bot.fetch_channel(channel_id)
+                giveaway_message = await channel.fetch_message(giveaway_id)
+                await giveaway_message.edit(embed=giveaway_ended, view=None)
+
+    def _get_winners(self, table: str, num_of_winners: int) -> list:
+        random = SystemRandom()
+
+        ga_cursor.execute(""f"SELECT discord_id, hsr_uid, entries FROM {table}""")
+        results = ga_cursor.fetchall()
+
+        user_pool, winners = [], []
+
+        for result in results:
+            user, entries = result[0], result[2]
+            for _ in range(entries):
+                user_pool.append(user)
+
+        num = 0
+        if user_pool:
+            for user in user_pool:
+                winners.append(random.choice(user_pool))
+                user_pool.remove(user)
+                num += 1
+                if num == num_of_winners:
+                    break
+
+        return winners
+
 
